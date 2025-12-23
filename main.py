0257a9f56ef377982b80456e2320a040affd2049
MikleS01 <168851474+MikleS01@users.noreply.github.com>
2025-12-23 19:42:14 +0300

    Adding picture error handled and added ReadMe

--- main.py
+++ main.py
@@ -23,16 +23,27 @@ def clear_folder(folder_path):
 
 
 
-url = "https://ru.wikipedia.org/wiki/%D0%95%D0%BB%D1%8C%D1%86%D0%B8%D0%BD,_%D0%91%D0%BE%D1%80%D0%B8%D1%81_%D0%9D%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D0%B5%D0%B2%D0%B8%D1%87"
+url = input("ВВедите ссылку на статью WIKI:") 
+while True:
+    if "https://ru.wikipedia.org/wiki/" in url:
+        print("Cсылка введeна успешно")
+        break
+    else:
+        print("Неправельная ссылка")
+
+    
 
 headers = {
-    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 14_6_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
 }
-
-response = requests.get(url, headers = headers)
+proxies={
+        "http": "http://npmtxnba-8:u16fw7al6u19@p.webshare.io:80/",
+        "https": "http://npmtxnba-8:u16fw7al6u19@p.webshare.io:80/"
+    } 
+response = requests.get(url, headers = headers,proxies = proxies)
 response.raise_for_status()
 soup = BeautifulSoup(response.text, features="html.parser")
- 
+
 Headline = soup.find("h1")
 all_tags = soup.find_all(['p', 'h2',"h3",'h4','h5','img'])
 
@@ -56,7 +67,7 @@ not_empty_tags = all_tags[0:-number]
 
 os.makedirs("WikiPhoto", exist_ok = True)
 clear_folder(folder_path = "WikiPhoto")   
-for number,tag in  enumerate(not_empty_tags):    
+for number,tag in enumerate(not_empty_tags):    
     clear_text = re.sub(r'\[\d*\]', ' ', tag. text)
     if "<p" in str(tag): 
         clear_paragaphs = doc.add_paragraph(clear_text)
@@ -78,13 +89,18 @@ for number,tag in  enumerate(not_empty_tags):
         head5.alignment = WD_ALIGN_PARAGRAPH.CENTER 
     elif "<img" in str(tag):
         img_src = 'https:' + tag["src"]
-        response = requests.get(img_src,headers = headers)
+        response = requests.get(img_src,headers = headers, proxies = proxies)
         response.raise_for_status()      
         with open(f'WikiPhoto/Photo{number}.png','wb') as file:
            file.write(response.content)
-        img2 = doc.add_picture(f'WikiPhoto/Photo{number}.png')
-        img2 = doc.paragraphs[-1] 
-        img2.alignment = WD_ALIGN_PARAGRAPH.CENTER
+        
+        try:
+            img2 = doc.add_picture(f'WikiPhoto/Photo{number}.png')
+            print(f'WikiPhoto/Photo{number}.png')
+            img2 = doc.paragraphs[-1] 
+            img2.alignment = WD_ALIGN_PARAGRAPH.CENTER
+        except:
+            print(f'WikiPhoto/Photo{number}.png' , "!!!!")
      
 doc.save('test.docx')
 
